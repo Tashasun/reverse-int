7abde9d090f43b2020151d223817e031a85e1757
tashasun <natalia.limbakh@gmail.com>
2021-08-15 19:58:57 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    // const reverse = (number) =>{
+        let digit = 1;
+        let result =""
+        if (number < 0) {number = number*-1}
+        while (number >= 1) {
+        digit = number % 10;
+        number = Math.floor(number / 10);
+        result = `${result}${digit}`;
+        }
+        return Number(result)
+      };
+// console.log(reverse(450));
+// console.log(reverse(-125));
